3492ec2feffed76724af1aa8f89b020a59a897d9
Martin Cao <martincao@pop-os.localdomain>
2020-09-24 19:12:24 +1000

    Add error handling to GformSponsorUsername.js

--- resources/assets/scripts/components/GformSponsorUsername.js
+++ resources/assets/scripts/components/GformSponsorUsername.js
@@ -8,22 +8,41 @@ export default class GformSponsorUsername {
   }
 
   async main(element) {
-    const affid = this.getAffid();
-    const response = await this.getUserByAffid(affid);
-    const sponsorUsername = await response.data.user_login;
-    await this.injectInputValue(element, sponsorUsername);
+    try {
+      const affid = this.getAffid();
+      const sponsorUsername = await this.getSponsorUsername(affid);
+      await this.injectInputValue(element, sponsorUsername);
+    } catch(e) {
+      console.warn(e);
+    }
   }
   
   getAffid() {
-    const fallbackAffid = 1
-    const uapCookie = Cookies.get('uap_referral');
-    const affid = uapCookie ? unserialize(uapCookie).affiliate_id : fallbackAffid;
+    const cookie = Cookies.get('uap_referral');
+
+    if (!cookie) {
+      throw '"uap_referral" cookie not found';
+    }
+
+    const affid = unserialize(cookie).affiliate_id;
     return affid;
   }
 
-  async getUserByAffid(affid) {
-    const response = await fetch(`/mos-api/user/affid/${affid}`);
-    return response.json();
+  async getSponsorUsername(affid) {
+    // Fetch sponsor's WP_User object via mos-api
+    try {
+      const response = await fetch(`/mos-api/user/affid/${affid}`);
+      var json_response = await response.json();
+    } catch(e) {
+      throw `Fetched a non-JSON response from /mos-api/user/affid/${affid}`;
+    }
+
+    // Get Username from WP_User object
+    try {
+      return json_response.data.user_login;
+    } catch {
+      throw `Could not get WP_User object from affid=${affid}`;
+    }
   }
 
   async injectInputValue(element, string) {
