c07001b47a327c8592c1d455549d6f2ba3a48593
Martin Cao <martin.g.cao@gmail.com>
2020-10-13 14:55:29 +1100

    Add actions.php to app

---
+++ app/actions.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+/**
+ * Add custom menu locations
+ */
+add_action( 'init', function() {
+  // register_nav_menus(
+  //   array(
+  //     'new-menu' => __( 'New Menu' ),
+  //     'another-menu' => __( 'Another Menu' ),
+  //     'an-extra-menu' => __( 'An Extra Menu' ),
+  //   )
+  // );
+});
