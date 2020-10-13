c151c7e41fe4bc51dac0cc9c25121f1f096847c3
Martin Cao <martin.g.cao@gmail.com>
2020-10-13 13:06:48 +1100

    Remove div wrapper around header nav

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,7 +2,7 @@
   <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
   <nav class="nav-primary">
     @if (has_nav_menu('primary_navigation'))
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'container' => false]) !!}
     @endif
   </nav>
 </header>
