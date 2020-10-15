d30b639c0b818726a6c199a9fda3cf696c70878c
Martin Cao <martin.g.cao@gmail.com>
2020-10-15 17:18:31 +1100

    Display mark complete button in leson template

--- resources/views/single-sfwd-topic.blade.php
+++ resources/views/single-sfwd-topic.blade.php
@@ -4,5 +4,6 @@
   @while(have_posts()) @php the_post() @endphp
     @include('partials.vimeo', ['video_id'=>$video])
     @include('partials.ld.course-nav')
+    @include('partials.ld.mark-complete')
   @endwhile
 @endsection
