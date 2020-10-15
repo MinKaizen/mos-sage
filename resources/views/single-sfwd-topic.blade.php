a8f69cd8141fe8f13c8dc431648f6e5dfc6ea74b
Martin Cao <martin.g.cao@gmail.com>
2020-10-15 15:24:00 +1100

    Remove debug prints and include ld course nav partial

--- resources/views/single-sfwd-topic.blade.php
+++ resources/views/single-sfwd-topic.blade.php
@@ -2,9 +2,6 @@
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
-    <h1>This is the single lesson template</h1>
-    <h2>This is a mark complete button:</h2>
-    {!! $mark_complete !!}
-    <pre>{!! print_r($nav_items) !!}</pre>
+    @include('partials.ld.course-nav')
   @endwhile
 @endsection
