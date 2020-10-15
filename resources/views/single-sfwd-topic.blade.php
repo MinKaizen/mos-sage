91d3a1a8359d697e2cbc219a65c54eb0ff7241aa
Martin Cao <martin.g.cao@gmail.com>
2020-10-15 17:12:04 +1100

    display video in lesson template

--- resources/views/single-sfwd-topic.blade.php
+++ resources/views/single-sfwd-topic.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
+    @include('partials.vimeo', ['video_id'=>$video])
     @include('partials.ld.course-nav')
   @endwhile
 @endsection
