6c2ceef12ba888081e423185e7b3268ad010dfbc
Martin Cao <martin.g.cao@gmail.com>
2020-10-16 10:12:36 +1100

    Only show video if video field exists

--- resources/views/single-sfwd-topic.blade.php
+++ resources/views/single-sfwd-topic.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
-    @include('partials.vimeo', ['video_id'=>$video])
+    @if($video) @include('partials.vimeo', ['video_id'=>$video]) @endif
     @include('partials.ld.course-nav')
     @include('partials.ld.mark-complete')
     @if(App\is_complete()) <h3>This lesson is complete!</h3> @endif
