7e6c88691857e91945a7cc0e0298119f3d418bfc
Martin Cao <martin.g.cao@gmail.com>
2020-10-16 13:20:05 +1100

    Remove temp prints

--- resources/views/single-sfwd-topic.blade.php
+++ resources/views/single-sfwd-topic.blade.php
@@ -2,11 +2,9 @@
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
-    <h1>Next lesson is:</h1>
-    <p>@dump($next_lesson)</p>
-    <h1>Previous lesson is:</h1>
-    <p>@dump($previous_lesson)</p>
     @if($video) @include('partials.vimeo', ['video_id'=>$video]) @endif
+    @include('partials.ld.prev-link')
+    @include('partials.ld.next-link')
     @include('partials.ld.course-nav')
     @include('partials.ld.mark-complete')
     @if(App\Learndash\is_complete()) <h3>This lesson is complete!</h3> @endif
