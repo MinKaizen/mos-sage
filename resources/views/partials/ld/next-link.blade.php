5f7db890b527b7cb8a97080fa6fe96c74aaf3ed5
Martin Cao <martin.g.cao@gmail.com>
2020-10-16 13:23:20 +1100

    Add next and previous link partials

---
+++ resources/views/partials/ld/next-link.blade.php
@@ -0,0 +1,3 @@
+@if($next_lesson)
+<span><a href="{!! $next_lesson['link'] !!}">Next >>></a></span>
+@endif
---
+++ resources/views/partials/ld/prev-link.blade.php
@@ -0,0 +1,3 @@
+@if($previous_lesson)
+<span><a href="{!! $previous_lesson['link'] !!}"><<< Previous</a></span>
+@endif
