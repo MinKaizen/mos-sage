3da04c6315741c91bad3533a925c4f5c66a1880a
Martin Cao <martin.g.cao@gmail.com>
2021-01-04 14:27:19 +1100

    Move complete button above navigation and remove if condition

--- resources/views/single-sfwd-topic.blade.php
+++ resources/views/single-sfwd-topic.blade.php
@@ -5,8 +5,8 @@
     @if(isset($video)) @include('partials.vimeo', ['video_id'=>$video]) @endif
     @include('partials.ld.prev-link')
     @include('partials.ld.next-link')
+    @include('partials.ld.mark-complete')
     @include('partials.ld.course-nav', ['course_structure' => $course_structure])
-    @if($is_complete) @include('partials.ld.mark-complete')@endif
     @include('partials.ld.resources')
   @endwhile
 @endsection
