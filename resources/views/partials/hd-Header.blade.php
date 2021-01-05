b921635a5c38f3d960146e04a1fc63e12e6bea8c
Martin Cao <martin.g.cao@gmail.com>
2021-01-06 08:23:46 +1100

    Include HeaderLogo partial in Header

--- resources/views/partials/hd-Header.blade.php
+++ resources/views/partials/hd-Header.blade.php
@@ -1,5 +1,5 @@
 <header class="hd-Header">
-  <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+  @include('partials.hd-HeaderLogo')
   @if (has_nav_menu('primary_navigation'))
     {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav-primary', 'container' => false]) !!}
   @endif
