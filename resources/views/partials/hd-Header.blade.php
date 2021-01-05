72da28a291eb833ae72ef86e9347d2833478ee0c
Martin Cao <martin.g.cao@gmail.com>
2021-01-06 08:34:15 +1100

    Factor out HeaderNav to its own partial

--- resources/views/partials/hd-Header.blade.php
+++ resources/views/partials/hd-Header.blade.php
@@ -1,6 +1,4 @@
 <header class="hd-Header">
   @include('partials.hd-HeaderLogo')
-  @if (has_nav_menu('primary_navigation'))
-    {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'hd-HeaderNav', 'container' => false]) !!}
-  @endif
+  @include('partials.hd-HeaderNav')
 </header>
