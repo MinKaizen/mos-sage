c99b16b3630db95a8b5b3700e08c55c37ec0f2a7
Martin Cao <martin.g.cao@gmail.com>
2021-01-06 08:23:15 +1100

    Fix indent

--- resources/views/partials/hd-Header.blade.php
+++ resources/views/partials/hd-Header.blade.php
@@ -1,6 +1,6 @@
 <header class="hd-Header">
   <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    @if (has_nav_menu('primary_navigation'))
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav-primary', 'container' => false]) !!}
-    @endif
+  @if (has_nav_menu('primary_navigation'))
+    {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav-primary', 'container' => false]) !!}
+  @endif
 </header>
