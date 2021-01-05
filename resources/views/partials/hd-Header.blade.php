435da3a6f71036bfe65e15211480af53f476a8a4
Martin Cao <martin.g.cao@gmail.com>
2021-01-06 08:33:16 +1100

    Rename nav-menu class to hd-HeaderNav

--- resources/views/partials/hd-Header.blade.php
+++ resources/views/partials/hd-Header.blade.php
@@ -1,6 +1,6 @@
 <header class="hd-Header">
   @include('partials.hd-HeaderLogo')
   @if (has_nav_menu('primary_navigation'))
-    {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav-primary', 'container' => false]) !!}
+    {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'hd-HeaderNav', 'container' => false]) !!}
   @endif
 </header>
