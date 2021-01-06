0173c67554fa3ab2510f32a435828bcb11cf6704
Martin Cao <martin.g.cao@gmail.com>
2021-01-07 08:23:42 +1100

    Rename Hamburger to just Burger

--- resources/views/partials/hd-HeaderNavMobile.blade.php
+++ resources/views/partials/hd-HeaderNavMobile.blade.php
@@ -1,8 +1,8 @@
 <div class="hd-HeaderNavMobile">
-  <span class="hd-HeaderNavMobile_Hamburger js-hd-HeaderNavMobile_Hamburger" data-nav-selector=".hd-HeaderNavMobile">
-    <div class="hd-HeaderNavMobile_HamburgerLine"></div>
-    <div class="hd-HeaderNavMobile_HamburgerLine"></div>
-    <div class="hd-HeaderNavMobile_HamburgerLine"></div>
+  <span class="hd-HeaderNavMobile_Burger js-hd-HeaderNavMobile_Burger" data-nav-selector=".hd-HeaderNavMobile">
+    <div class="hd-HeaderNavMobile_BurgerLine"></div>
+    <div class="hd-HeaderNavMobile_BurgerLine"></div>
+    <div class="hd-HeaderNavMobile_BurgerLine"></div>
   </span>
   @if (has_nav_menu($menu_slug))
     {!! wp_nav_menu(['theme_location' => $menu_slug, 'menu_class' => 'hd-HeaderNavMobile_Menu', 'container' => false]) !!}
