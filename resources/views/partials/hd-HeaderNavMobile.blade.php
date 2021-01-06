c8056c8ba41b0223b210d0ec687bd28503a60b7d
Martin Cao <martin.g.cao@gmail.com>
2021-01-07 08:20:43 +1100

    Add mobile hamburger menu markup

--- resources/views/partials/hd-HeaderNavMobile.blade.php
+++ resources/views/partials/hd-HeaderNavMobile.blade.php
@@ -1,3 +1,10 @@
-@if (has_nav_menu($menu_slug))
-  {!! wp_nav_menu(['theme_location' => $menu_slug, 'menu_class' => 'hd-HeaderNavMobile', 'container' => false]) !!}
-@endif
+<div class="hd-HeaderNavMobile">
+  <span class="hd-HeaderNavMobile_Hamburger js-hd-HeaderNavMobile_Hamburger" data-nav-selector=".hd-HeaderNavMobile">
+    <div class="hd-HeaderNavMobile_HamburgerLine"></div>
+    <div class="hd-HeaderNavMobile_HamburgerLine"></div>
+    <div class="hd-HeaderNavMobile_HamburgerLine"></div>
+  </span>
+  @if (has_nav_menu($menu_slug))
+    {!! wp_nav_menu(['theme_location' => $menu_slug, 'menu_class' => 'hd-HeaderNavMobile_Menu', 'container' => false]) !!}
+  @endif
+</div>
