613014775ef9734cfdd10fe3ada5abcd53714876
Martin Cao <martin.g.cao@gmail.com>
2021-01-07 13:32:46 +1100

    Remove debug console logs

--- resources/assets/scripts/components/ClassToggler.js
+++ resources/assets/scripts/components/ClassToggler.js
@@ -3,8 +3,6 @@ export default class ClassToggler {
     const targetElement = document.querySelector(element.dataset.targetSelector)
     const toggleClass = element.dataset.toggleClass
     element.addEventListener('click', () => {
-      console.log(`${toggleClass}, ${targetElement}`)
-      console.log(targetElement)
       targetElement.classList.toggle(toggleClass)
     })
   }
