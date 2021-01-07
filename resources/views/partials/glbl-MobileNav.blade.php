37334b03a5cf24ef8307d794b522225bdb6f8677
Martin Cao <martin.g.cao@gmail.com>
2021-01-07 14:27:45 +1100

    Refactor MobileNav_Drawer markup

--- resources/views/partials/glbl-MobileNav.blade.php
+++ resources/views/partials/glbl-MobileNav.blade.php
@@ -4,14 +4,15 @@
   {{-- Drawer --}}
   <div class="glbl-MobileNav_Drawer">
 
-    <span class="glbl-MobileNav_Close js-ClassToggler"
-          data-target-selector=".glbl-MobileNav"
-          data-toggle-class="is-Active">
-      <div class="glbl-MobileNav_Close_Line1"></div>
-      <div class="glbl-MobileNav_Close_Line2"></div>
-    </span>
-
-    <a href="{{ home_url('/') }}"><img src="@asset('images/logo-type-light.png')" alt="My Online Startup"></a>
+    <div class="glbl-MobileNav_Drawer_Top">
+      <a href="{{ home_url('/') }}"><img src="@asset('images/logo-icon-40x36.png')" alt="My Online Startup"></a>
+      <span class="glbl-MobileNav_Close js-ClassToggler"
+            data-target-selector=".glbl-MobileNav"
+            data-toggle-class="is-Active">
+        <div class="glbl-MobileNav_Close_Line1"></div>
+        <div class="glbl-MobileNav_Close_Line2"></div>
+      </span>
+    </div>
 
     @if (has_nav_menu($menu_slug))
       {!! wp_nav_menu(['theme_location' => $menu_slug, 'menu_class' => 'glbl-MobileNav_Menu', 'container' => false]) !!}
