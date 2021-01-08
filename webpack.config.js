dac80cfd053cf411cfb0f2256b5e600c2fc01196
Martin Cao <martin.g.cao@gmail.com>
2021-01-08 13:16:10 +1100

    Configure copy files with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: 'development',
@@ -11,5 +12,19 @@ module.exports = {
   watchOptions: {
     aggregateTimeout: 200,
     poll: 1000
-  }
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'resources/assets/fonts'),
+          to: path.resolve(__dirname, 'dist/fonts'),
+          globOptions: {
+            dot: false,
+            gitignore: true,
+          },
+        },
+      ],
+    }),
+  ],
 };
