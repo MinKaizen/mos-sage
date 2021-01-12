e3ab16104f60cdcad68a85df9c5351f73f3b709b
Martin Cao <martin.g.cao@gmail.com>
2021-01-13 09:07:37 +1100

    Split ldcp-Fraction into separate 3 spans

--- resources/views/partials/ld/ldcp-CourseProgress.blade.php
+++ resources/views/partials/ld/ldcp-CourseProgress.blade.php
@@ -2,5 +2,10 @@
   <div class="ldcp-Track">
     <div class="ldcp-Fill" style="width: {{ $course_progress['percentage_str'] }};"></div>
   </div>
-  <span class="ldcp-Fraction">{{ $course_progress['formatted'] }}</span>
+  <span class="ldcp-Fraction"
+        style="left: {{ $course_progress['percentage_str'] }}">
+    <span class="ldcp-Fraction_Completed">{{ $course_progress['completed'] }}</span>
+    <span class="ldcp-Fraction_Divider">/</span>
+    <span class="ldcp-Fraction_Total">{{ $course_progress['total'] }}</span>
+  </span>
 </div>
