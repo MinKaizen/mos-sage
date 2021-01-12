de504038a5a3b4605e1956a98faee7ff519916b1
Martin Cao <martin.g.cao@gmail.com>
2021-01-13 08:53:07 +1100

    Add top padding to ldsb-Sidebar

--- resources/views/partials/ld/ldcp-CourseProgress.blade.php
+++ resources/views/partials/ld/ldcp-CourseProgress.blade.php
@@ -1,5 +1,5 @@
 <div class="ldcp-CourseProgress">
-  <div class="ldcp-Container">
+  <div class="ldcp-Track">
     <div class="ldcp-Fill" style="width: {{ $course_progress['percentage_str'] }};"></div>
   </div>
   <span class="ldcp-Fraction">{{ $course_progress['formatted'] }}</span>
