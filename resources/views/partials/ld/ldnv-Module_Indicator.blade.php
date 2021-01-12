511ec2c6f1c27d39050f0607e4092431389cb88c
Martin Cao <martin.g.cao@gmail.com>
2021-01-12 14:57:23 +1100

    Add classes to ldnv-Module_Indicator

--- resources/views/partials/ld/ldnv-Module_Indicator.blade.php
+++ resources/views/partials/ld/ldnv-Module_Indicator.blade.php
@@ -1,5 +1,5 @@
 @if($complete)
-  <img src="@asset('images/icon-lesson-complete-hollow.png')" alt="(Complete)">
+  <img class="ldnv-Module_Indicator ldnv-Module_Indicator-complete" src="@asset('images/icon-lesson-complete-hollow.png')" alt="(Complete)">
 @else
-  <img src="@asset('images/icon-lesson-incomplete-hollow.png')" alt="(Incomplete)">
+  <img class="ldnv-Module_Indicator ldnv-Module_Indicator-incomplete" src="@asset('images/icon-lesson-incomplete-hollow.png')" alt="(Incomplete)">
 @endif
