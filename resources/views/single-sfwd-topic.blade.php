2e6301c1f24fe3e0d708e92f381212bb06514ac2
Martin Cao <martin.g.cao@gmail.com>
2021-01-12 13:00:09 +1100

    FIX: name section ld-main instead of ld-content

--- resources/views/single-sfwd-topic.blade.php
+++ resources/views/single-sfwd-topic.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.ld-lesson')
 
-@section('ld-content')
+@section('ld-main')
   @while(have_posts()) @php the_post() @endphp
     @if(isset($video)) @include('partials.vimeo', ['video_id'=>$video]) @endif
     @include('partials.ld.prev-link')
