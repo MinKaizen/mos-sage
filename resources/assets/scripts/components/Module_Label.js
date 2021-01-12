715682bfddc8e8ba24fd2acfbc36f02d4279ae84
Martin Cao <martin.g.cao@gmail.com>
2021-01-12 15:47:14 +1100

    Implement js component Module_Label

--- resources/assets/scripts/components/Module_Label.js
+++ resources/assets/scripts/components/Module_Label.js
@@ -1,5 +1,9 @@
 export default class Module_Label {
   constructor(moduleLabelElement) {
-    console.log(moduleLabelElement)
+    const lessonsElement = moduleLabelElement.parentNode.querySelector('.ldnv-Module_Lessons')
+    moduleLabelElement.addEventListener('click', (event) => {
+      event.preventDefault()
+      lessonsElement.classList.toggle('is-Active')
+    })
   }
 }
