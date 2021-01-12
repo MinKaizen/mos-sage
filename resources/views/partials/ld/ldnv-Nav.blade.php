29f6b50edb3ae397071755ae032125e326c48efd
Martin Cao <martin.g.cao@gmail.com>
2021-01-12 16:18:47 +1100

    Pass variable id to ldnv-Module

--- resources/views/partials/ld/ldnv-Nav.blade.php
+++ resources/views/partials/ld/ldnv-Nav.blade.php
@@ -1,6 +1,7 @@
 <div class="ldnv-Nav">
 @foreach($course_structure as $module)
   @include('partials.ld.ldnv-Module', [
+    'id' => $module->ID,
     'module_num' => $loop->iteration,
     'title' => $module->post_title,
     'lessons' => $module->lessons,
