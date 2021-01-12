56510f48f5235ea9187ff1dac14d361f8b2c999a
Martin Cao <martin.g.cao@gmail.com>
2021-01-13 09:16:36 +1100

    Rename ldnv-Toggle to ldnv-Sidebar_Toggle

--- resources/views/partials/ld/ldsb-Sidebar.blade.php
+++ resources/views/partials/ld/ldsb-Sidebar.blade.php
@@ -1,5 +1,5 @@
 <aside class="ldsb-Sidebar">
   @include('partials.ld.ldcp-CourseProgress', ['course_progress' => $course_progress])
   @include('partials.ld.ldnv-Nav', ['course_structure' => $course_structure])
-  <span class="ldnv-Toggle"></span>
+  <span class="ldnv-Sidebar_Toggle"></span>
 </aside>
