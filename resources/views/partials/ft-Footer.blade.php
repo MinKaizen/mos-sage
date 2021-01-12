3bfe667b04ae92b34ef1f5bc1fe7bb8110f7fbb5
Martin Cao <martin.g.cao@gmail.com>
2021-01-12 12:57:19 +1100

    Add partial ft-Footer

---
+++ resources/views/partials/ft-Footer.blade.php
@@ -0,0 +1,7 @@
+@php do_action('get_footer') @endphp
+<footer class="ft-Footer">
+  <div class="ft-Footer_Container">
+    @php dynamic_sidebar('sidebar-footer') @endphp
+  </div>
+</footer>
+@php wp_footer() @endphp
