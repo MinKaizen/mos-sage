a4866f1ef42353a140162df4de7b41a4405733e6
Martin Cao <martin.g.cao@gmail.com>
2021-01-12 12:48:23 +1100

    Remove ldsb-Sidebar from  single-sfwd-topic.blade.php

--- resources/views/single-sfwd-topic.blade.php
+++ resources/views/single-sfwd-topic.blade.php
@@ -6,7 +6,6 @@
     @include('partials.ld.prev-link')
     @include('partials.ld.next-link')
     @include('partials.ld.mark-complete')
-    @include('partials.ld.ldsb-Sidebar')
     @include('partials.ld.resources')
   @endwhile
 @endsection
