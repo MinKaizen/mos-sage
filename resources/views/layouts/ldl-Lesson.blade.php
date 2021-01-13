8a08a70ff2b7bab50aa44ad2e6ff7a80711f1655
Martin Cao <martin.g.cao@gmail.com>
2021-01-14 09:26:27 +1100

    Replace ldm-SidebarToggle with ldsbt-SidebarToggle

--- resources/views/layouts/ldl-Lesson.blade.php
+++ resources/views/layouts/ldl-Lesson.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.main')
 
 @section('content')
+  @include('partials.ld.ldsbt-SidebarToggle')
   <div class="ldl-Lesson">
     @include('partials.ld.ldsb-Sidebar')
     <div class="ldm-Main">
-      @include('partials.ld.ldm-SidebarToggle')
+      {{-- @include('partials.ld.ldm-SidebarToggle') --}}
       @yield('ldm-Main')
     </div>
   </div>
