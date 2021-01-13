6672564012d9bcd592b150a61481854a5a074c9e
Martin Cao <martin.g.cao@gmail.com>
2021-01-13 12:18:04 +1100

    Remove unused element

--- resources/views/partials/ld/ldsb-Sidebar.blade.php
+++ resources/views/partials/ld/ldsb-Sidebar.blade.php
@@ -1,5 +1,4 @@
 <aside class="ldsb-Sidebar">
   @include('partials.ld.ldcp-CourseProgress', ['course_progress' => $course_progress])
   @include('partials.ld.ldnv-Nav', ['course_structure' => $course_structure])
-  <span class="ldsb-Sidebar_Toggle"></span>
 </aside>
