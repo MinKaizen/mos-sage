c2f407a8f50be7db57a21527d2c97fd1fb3334ba
Martin Cao <martin.g.cao@gmail.com>
2021-01-14 10:54:55 +1100

    Wrap next and prev buttons in a _Nav component

--- resources/views/partials/ld/ldvi-VideoIsland.blade.php
+++ resources/views/partials/ld/ldvi-VideoIsland.blade.php
@@ -2,7 +2,8 @@
   @if(isset($video_url))
     @include('partials.vid-Video', ['url' => $video_url])
   @endif
-  @include('partials.ld.prev-link')
-  @include('partials.ld.next-link')
-  @include('partials.ld.mark-complete')
+  <div class="ldvi-VideoIsland_Nav">
+    @include('partials.ld.prev-link')
+    @include('partials.ld.next-link')
+  </div>
 </div>
