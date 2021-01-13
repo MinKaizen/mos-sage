f7eabbf2cafd21683745b16b132aa2798f7739d7
Martin Cao <martin.g.cao@gmail.com>
2021-01-14 10:35:18 +1100

    Revert "Wrap next and previous link in ldvi-VideoIsland_Nav"
    This reverts commit 82325a1ba6b09fb32c62edc2b48d425ee3f5e94f.

--- resources/views/partials/ld/ldvi-VideoIsland.blade.php
+++ resources/views/partials/ld/ldvi-VideoIsland.blade.php
@@ -2,8 +2,7 @@
   @if(isset($video_url))
     @include('partials.vid-Video', ['url' => $video_url])
   @endif
-  <div class="ldvi-VideoIsland_Nav">
-    @include('partials.ld.prev-link')
-    @include('partials.ld.next-link')
-  </div>
+  @include('partials.ld.prev-link')
+  @include('partials.ld.next-link')
+  @include('partials.ld.mark-complete')
 </div>
