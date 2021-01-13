f23b186dd92d8fe8888906e2a4bd7a865389776f
Martin Cao <martin.g.cao@gmail.com>
2021-01-13 16:12:07 +1100

    Add sidebars mos_sidebar and affiliate_sidebar

--- app/actions.php
+++ app/actions.php
@@ -12,3 +12,29 @@ add_action( 'template_redirect', function() {
     redirect_module( \get_the_ID() );
   }
 });
+
+add_action( 'widgets_init', __NAMESPACE__ . '\register_sidebars' );
+function register_sidebars() {
+    register_sidebar(
+        [
+            'id'            => 'mos_sidebar',
+            'name'          => __( 'MOS Sidebar' ),
+            'description'   => __( 'Shows an Upgrade banner and other MOS related banners depending on user level.' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        ]
+    );
+    register_sidebar(
+        [
+            'id'            => 'affiliate_sidebar',
+            'name'          => __( 'Affiliate Sidebar' ),
+            'description'   => __( 'A bunch of 3rd party affiliate banners.' ),
+            'before_widget' => '<div id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+            'after_title'   => '</h3>',
+        ]
+    );
+}
