ea10e135f908a332454a8afb5f095840e455a714
Martin Cao <martin.g.cao@gmail.com>
2021-01-13 15:32:52 +1100

    Use new vid-Video partial in single-sfwd-topic

--- resources/views/single-sfwd-topic.blade.php
+++ resources/views/single-sfwd-topic.blade.php
@@ -2,7 +2,9 @@
 
 @section('ldm-Main')
   @while(have_posts()) @php the_post() @endphp
-    @if(isset($video)) @include('partials.vimeo', ['video_id'=>$video]) @endif
+    @if(isset($video_url))
+      @include('partials.vid-Video', ['url' => $video_url])
+    @endif
     @include('partials.ld.prev-link')
     @include('partials.ld.next-link')
     @include('partials.ld.mark-complete')
