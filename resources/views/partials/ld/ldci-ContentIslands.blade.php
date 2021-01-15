a70dddd806d9d30e317c0260e56d70e6023bb988
Martin Cao <martin.g.cao@gmail.com>
2021-01-15 11:59:49 +1100

    Include faq and action plan in ldci-ContentIslands

--- resources/views/partials/ld/ldci-ContentIslands.blade.php
+++ resources/views/partials/ld/ldci-ContentIslands.blade.php
@@ -1,9 +1,20 @@
 <div class="ldci-ContentIslands">
+
+  {{-- Action Plan --}}
+  @if(isset($action_plan) && $action_plan)
+    @include('partials.ld.ldap-ActionPlan', ['action_plan' => $action_plan])
+  @endif
+
+  {{-- Text Island --}}
   @if(get_the_content())
     @component('components.ti-TextIsland')
       {!! get_the_content() !!}
     @endcomponent
   @endif
-  {{-- If resources, print resources --}}
-  {{-- @include('partials.ld.resources') --}}
+
+  {{-- FAQ --}}
+  @if(isset($faq) && $faq)
+    @include('partials.ld-ldfaq-FAQ', ['faq' => $faq])
+  @endif
+
 </div>
