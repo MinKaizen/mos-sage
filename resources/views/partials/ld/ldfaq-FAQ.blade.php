3267b5531d32b7620b6219ccfd2c95149532a2cb
Martin Cao <martin.g.cao@gmail.com>
2021-01-15 11:59:12 +1100

    Add ldfaq-FAQ

---
+++ resources/views/partials/ld/ldfaq-FAQ.blade.php
@@ -0,0 +1,10 @@
+<div class="ldfaq-FAQ">
+  <h1>Frequently Asked Questions</h1>
+  <code>Data: @php var_dump($action_plan) @endphp</code>
+  <h4 class="ldfaq-Question">How do we do this?</h4>
+  <p class="ldfaq-Answer">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero earum eum magnam at dignissimos. Quos nemo ex error exercitationem, autem enim quia debitis iure eius consequatur dolore nostrum</p>
+  <h4 class="ldfaq-Question">Another question?</h4>
+  <p class="ldfaq-Answer">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero earum eum magnam at dignissimos. Quos nemo ex error exercitationem, autem enim quia debitis iure eius consequatur dolore nostrum</p>
+  <h4 class="ldfaq-Question">How do I get started?</h4>
+  <p class="ldfaq-Answer">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero earum eum magnam at dignissimos. Quos nemo ex error exercitationem, autem enim quia debitis iure eius consequatur dolore nostrum</p>
+</div>
