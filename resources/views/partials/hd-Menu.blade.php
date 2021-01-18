7af8bdb902bdb8fafba1d79b2df47b0be4d4a9c0
Martin Cao <martin.g.cao@gmail.com>
2021-01-19 09:08:26 +1100

    Refactor hd-Menu to use HeaderMenuWalker and custom class names

--- resources/views/partials/hd-Menu.blade.php
+++ resources/views/partials/hd-Menu.blade.php
@@ -1,3 +1,3 @@
 @if (has_nav_menu($menu_slug))
-  {!! wp_nav_menu(['theme_location' => $menu_slug, 'menu_class' => 'hd-Menu', 'container' => false]) !!}
+  {!! wp_nav_menu(['theme_location' => $menu_slug, 'menu_class' => 'hd-Menu', 'container' => false, 'walker' => $header_menu_walker]) !!}
 @endif
