16fda4e1ec0360edee09df68be7f576335cf9aee
Martin Cao <martin.g.cao@gmail.com>
2021-01-20 07:51:07 +1100

    Move sidebar registration to setup.php

--- app/setup.php
+++ app/setup.php
@@ -90,6 +90,51 @@ add_action('widgets_init', function () {
         'name'          => __('Footer', 'sage'),
         'id'            => 'sidebar-footer'
     ] + $config);
+    register_sidebar([
+        'id'            => 'monthly_partner_sidebar',
+        'name'          => __( 'Monthly Partner Sidebar' ),
+        'description'   => __( 'MOS related banners for Monthly Partners' ),
+        'before_widget' => '<section class="msb-MosSidebar_Banner">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<title class="msb-MosSidebar_Title">',
+        'after_title'   => '</title>',
+    ]);
+    register_sidebar([
+        'id'            => 'yearly_partner_sidebar',
+        'name'          => __( 'Yearly Partner Sidebar' ),
+        'description'   => __( 'MOS related banners for Yearly Partners' ),
+        'before_widget' => '<section class="msb-MosSidebar_Banner">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<title class="msb-MosSidebar_Title">',
+        'after_title'   => '</title>',
+    ]);
+    register_sidebar([
+        'id'            => 'lifetime_partner_sidebar',
+        'name'          => __( 'Lifetime Partner Sidebar' ),
+        'description'   => __( 'MOS related banners for Lifetime Partners' ),
+        'before_widget' => '<section class="msb-MosSidebar_Banner">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<title class="msb-MosSidebar_Title">',
+        'after_title'   => '</title>',
+    ]);
+    register_sidebar([
+        'id'            => 'coaching_sidebar',
+        'name'          => __( 'Coaching Sidebar' ),
+        'description'   => __( 'MOS related banners for Coaching Members' ),
+        'before_widget' => '<section class="msb-MosSidebar_Banner">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<title class="msb-MosSidebar_Title">',
+        'after_title'   => '</title>',
+    ]);
+    register_sidebar([
+        'id'            => 'affiliate_sidebar',
+        'name'          => __( 'Affiliate Sidebar' ),
+        'description'   => __( 'A bunch of 3rd party affiliate banners.' ),
+        'before_widget' => '<div class="asb-AffSidebar_Banner">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<title class="asb-AffSidebar_Title">',
+        'after_title'   => '</title>',
+    ]);
 });
 
 /**
