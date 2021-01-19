93fe59dab80345827bd97015eea36f7837c6620f
Martin Cao <martin.g.cao@gmail.com>
2021-01-20 08:26:48 +1100

    Disable Sage default Primary Navigation menu

--- app/setup.php
+++ app/setup.php
@@ -43,9 +43,9 @@ add_action('after_setup_theme', function () {
      * Register navigation menus
      * @link https://developer.wordpress.org/reference/functions/register_nav_menus/
      */
-    register_nav_menus([
-        'primary_navigation' => __('Primary Navigation', 'sage')
-    ]);
+    // register_nav_menus([
+    //     'primary_navigation' => __('Primary Navigation', 'sage')
+    // ]);
     register_nav_menus([
         'logged_out' => __( 'Logged Out Primary Naviation' ),
     ]);
