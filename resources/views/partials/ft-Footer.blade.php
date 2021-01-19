2bdff8c56dfc32f7d0ab0053ecd75abb355a4236
Martin Cao <martin.g.cao@gmail.com>
2021-01-19 14:18:17 +1100

    Update markup for ft-Footer partial

--- resources/views/partials/ft-Footer.blade.php
+++ resources/views/partials/ft-Footer.blade.php
@@ -1,7 +1,8 @@
 @php do_action('get_footer') @endphp
 <footer class="ft-Footer">
-  <div class="ft-Footer_Container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
-  </div>
+  @if (has_nav_menu('footer'))
+    {!! wp_nav_menu(['theme_location' => 'footer', 'menu_class' => 'ft-Menu', 'container' => false, 'walker' => $footer_menu_walker]) !!}
+  @endif
+  <span class="ft-Copyright">Copyright ©{{ date('Y') }} My Online Startup</span>
 </footer>
 @php wp_footer() @endphp
