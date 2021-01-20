86dea958c0029a74952c03de86727cf60bafd18b
Martin Cao <martin.g.cao@gmail.com>
2021-01-21 08:23:19 +1100

    Show a different module indicator depending on ACF field module_indicator in ldnv-Module_Indicator

--- resources/views/partials/ld/ldnv-Module_Indicator.blade.php
+++ resources/views/partials/ld/ldnv-Module_Indicator.blade.php
@@ -1,5 +1,9 @@
-@if($complete)
-  <img class="ldnv-Module_Indicator ldnv-Module_Indicator-complete" src="@asset('images/icon-lesson-complete-hollow.png')" alt="(Complete)">
+@if($module_indicator == 'partner')
+  <img class="ldnv-Module_Indicator ldnv-Module_Indicator-partner" src="@asset('images/icon-partner-module-hollow.png')">
 @else
-  <img class="ldnv-Module_Indicator ldnv-Module_Indicator-incomplete" src="@asset('images/icon-lesson-incomplete-hollow.png')" alt="(Incomplete)">
+  @if($complete)
+    <img class="ldnv-Module_Indicator ldnv-Module_Indicator-complete" src="@asset('images/icon-lesson-complete-hollow.png')" alt="(Complete)">
+  @else
+    <img class="ldnv-Module_Indicator ldnv-Module_Indicator-incomplete" src="@asset('images/icon-lesson-incomplete-hollow.png')" alt="(Incomplete)">
+  @endif
 @endif
