2a335b0577ff0160aaf23787f6fa9bf5360aa086
Martin Cao <martin.g.cao@gmail.com>
2021-01-21 08:06:03 +1100

    Conditionally show ldcp-CourseProgress

--- resources/views/partials/ld/ldsb-Sidebar.blade.php
+++ resources/views/partials/ld/ldsb-Sidebar.blade.php
@@ -1,4 +1,6 @@
 <aside class="ldsb-Sidebar">
-  @include('partials.ld.ldcp-CourseProgress', ['course_progress' => $course_progress])
+  @if($show_progress)
+    @include('partials.ld.ldcp-CourseProgress', ['course_progress' => $course_progress])
+  @endif
   @include('partials.ld.ldnv-Nav', ['course_structure' => $course_structure])
 </aside>
