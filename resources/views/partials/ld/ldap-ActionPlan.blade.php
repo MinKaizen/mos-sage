534ac0370c3a65e5300eeb33eddfda824c386985
Martin Cao <martin.g.cao@gmail.com>
2021-01-21 11:18:04 +1100

    Update markup for ldap-ActionPlan

--- resources/views/partials/ld/ldap-ActionPlan.blade.php
+++ resources/views/partials/ld/ldap-ActionPlan.blade.php
@@ -1,4 +1,17 @@
 <div class="ldap-ActionPlan">
-  <h1>Action Plan</h1>
-  <code>Data: @php var_dump($action_plan) @endphp</code>
+
+  <h2 class="ldap-ActionPlan_Title">Action Plan</h2>
+
+  @foreach($action_plan as $action_item)
+    @include('partials.ld.ldap-ActionItem', [
+      'title' => isset($action_item->title) ? $action_item->title : '',
+      'description' => isset($action_item->description) ? $action_item->description : '',
+      'cta' => isset($action_item->cta) ? $action_item->cta : (object) [
+        'color' => 'red',
+        'text' => 'Button Text',
+        'url' => '/',
+      ],
+    ])
+  @endforeach
+
 </div>
