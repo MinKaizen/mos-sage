c0b8cfc94ad11e08c22d0a3089a2e5f1dc758afe
Martin Cao <martin.g.cao@gmail.com>
2021-01-22 16:10:35 +1100

    Implement js component AfflinkCopy

--- resources/assets/scripts/components/AfflinkCopy.js
+++ resources/assets/scripts/components/AfflinkCopy.js
@@ -1,5 +1,27 @@
+const TEXT_SELECTOR = '.ldvi-Afflink_TextToCopy'
+const COPIED_TEXT = 'Copied!'
 export default class {
   constructor(copyElement) {
-    console.log(copyElement)
+    copyElement.addEventListener('click', () => {
+      main(copyElement)
+    })
   }
+}
+
+async function main(copyElement) {
+  const textElement = await getTextElement()
+  copyText(textElement)
+  changeText(copyElement, COPIED_TEXT)
+}
+
+async function getTextElement() {
+  return document.querySelector(TEXT_SELECTOR)
+}
+
+async function copyText(element) {
+  await navigator.clipboard.writeText(element.textContent);
+}
+
+async function changeText(element, text) {
+  element.textContent = text
 }
