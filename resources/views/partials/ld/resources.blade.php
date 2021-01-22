c824420017c0e99c48b47703126523aa257db290
Martin Cao <martin.g.cao@gmail.com>
2021-01-22 17:45:34 +1100

    Delete ld.resources.blade

--- resources/views/partials/ld/resources.blade.php
+++
@@ -1,9 +0,0 @@
-<div x-data="{ tab: 'action_plan' }">
-  <button :class="{ 'active': tab === 'action_plan' }" @click="tab = 'action_plan'">Action plan</button>
-  <button :class="{ 'active': tab === 'quiz' }" @click="tab = 'quiz'">Quiz</button>
-  <button :class="{ 'active': tab === 'notes' }" @click="tab = 'notes'">Notes</button>
-
-  <div x-show="tab === 'action_plan'">Action Plan Tab</div>
-  <div x-show="tab === 'quiz'">Quiz Tab</div>
-  <div x-show="tab === 'notes'">Notes Tab</div>
-</div>
