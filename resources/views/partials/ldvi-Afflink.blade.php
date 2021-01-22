605e3c102b16655e4fe682aaf3d3e0e441fb9ae6
Martin Cao <martin.g.cao@gmail.com>
2021-01-22 15:14:35 +1100

    Add component ldvi-Afflink

---
+++ resources/views/partials/ldvi-Afflink.blade.php
@@ -0,0 +1,5 @@
+<div class="ldvi-Afflink">
+  <img class="ldvi-Afflink_Icon" src="@asset('images/icon-link-outline-gradient.svg')">
+  <div class="ldvi-Afflink_Text">Your Referral Link:</div>
+  <div class="ldvi-Afflink_Copy">Copy</div>
+</div>
