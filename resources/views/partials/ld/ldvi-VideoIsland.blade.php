8740cc98e5d0f4ccd0fe5c1fd6331cfa6f8ff162
Martin Cao <martin.g.cao@gmail.com>
2021-01-22 12:14:12 +1100

    Disable download-pdf while we wait for business decision

--- resources/views/partials/ld/ldvi-VideoIsland.blade.php
+++ resources/views/partials/ld/ldvi-VideoIsland.blade.php
@@ -2,10 +2,10 @@
   @if(isset($video_url))
     @include('partials.vid-Video', ['url' => $video_url])
   @endif
-  <a href="#">
+  {{-- <a href="#">
     <span class="ldvi-DownloadPdf">
       <img class="ldvi-DownloadPdf_Icon" src="@asset('images/icon-pdf-hollow.svg')">
       Download PDF
     </span>
-  </a>
+  </a> --}}
 </div>
