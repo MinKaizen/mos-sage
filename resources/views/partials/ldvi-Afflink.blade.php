a15aad16609c6eefeb480c65ddc26ae808b1ee2f
Martin Cao <martin.g.cao@gmail.com>
2021-01-22 17:18:45 +1100

    Insert dynamic afflink in ldvi-Afflink

--- resources/views/partials/ldvi-Afflink.blade.php
+++ resources/views/partials/ldvi-Afflink.blade.php
@@ -1,5 +1,5 @@
 <div class="ldvi-Afflink">
   <img class="ldvi-Afflink_Icon" src="@asset('images/icon-link-outline-gradient.svg')">
-  <div class="ldvi-Afflink_Text">Your Referral Link: <span class="ldvi-Afflink_TextToCopy">https://google.com</span></div>
+  <div class="ldvi-Afflink_Text">Your Referral Link: <span class="ldvi-Afflink_TextToCopy">{{ $afflink }}</span></div>
   <div class="ldvi-Afflink_Copy js-AfflinkCopy">Copy</div>
 </div>
