fbca424ec96118382fb71423d23af9bca2b9b02d
Martin Cao <martin.g.cao@gmail.com>
2021-01-25 16:45:07 +1100

    Add dummy markup to show totals in view template-my-commissions

--- resources/views/template-my-commissions.blade.php
+++ resources/views/template-my-commissions.blade.php
@@ -5,5 +5,8 @@
 @extends('layouts.main')
 
 @section('content')
+  <h1>Total Commissions: {{ $total_commissions }}</h1>
+  <h1>Total Referrals: {{ $total_referrals }}</h1>
+  <h1>Total EPR: {{ $total_epr }}</h1>
   @include('partials.dt-Commissions')
 @endsection
