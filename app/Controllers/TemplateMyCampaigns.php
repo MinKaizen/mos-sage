f4b4a2cb7150085e1bc9758b7a3960c53bff554a
Martin Cao <martin.g.cao@gmail.com>
2021-01-25 15:05:05 +1100

    Add empty controller TemplateMyCampaigns

---
+++ app/Controllers/TemplateMyCampaigns.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+use Sober\Controller\Controller;
+
+class TemplateMyCampaigns extends Controller
+{
+    public function campaigns() {
+        return [];
+    }
+}
