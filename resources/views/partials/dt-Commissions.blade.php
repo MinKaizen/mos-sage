b6ceddf64a2617d9d8ccc1864fec5a2fdddf2c8d
Martin Cao <martin.g.cao@gmail.com>
2021-01-25 15:57:54 +1100

    FIX: markup syntax error for dt-Commissions

--- resources/views/partials/dt-Commissions.blade.php
+++ resources/views/partials/dt-Commissions.blade.php
@@ -1,4 +1,4 @@
-<table class="js-commissions-table">
+<table class="js-MosTable">
   <thead class="table-header">
     <tr class="table-header__row">
       <th class="table-cell table-header-cell col-date">Date</th>
@@ -13,14 +13,14 @@
   <tbody class="table-body">
     @foreach( $rows as $row )
     <tr class="table-body__row">
-      <td class="table-cell table-body-cell col-date">{{ $row['date']; }}</td>
-      <td class="table-cell table-body-cell col-amount @if((int) $row['amount'] < 0 ) col-amount--negative @endif ">{{ format_currency( (float) $row['amount'], 0 ); }}</td>
-      <td class="table-cell table-body-cell col-name">{{ ucwords( strtolower( $row['display_name'] ) ); }}</td>
-      <td class="table-cell table-body-cell col-email">{{ strtolower( $row['user_email'] ); }}</td>
-      <td class="table-cell table-body-cell col-product">{{ $row['description']; }}</td>
-      <td class="table-cell table-body-cell col-campaign">{{ strtolower( $row['campaign'] ); }}</td>
+      <td class="table-cell table-body-cell col-date">{{ $row['date'] }}</td>
+      <td class="table-cell table-body-cell col-amount @if((int) $row['amount'] < 0 ) col-amount--negative @endif ">{{ format_currency( (float) $row['amount'], 0 ) }}</td>
+      <td class="table-cell table-body-cell col-name">{{ ucwords( strtolower( $row['display_name'] ) ) }}</td>
+      <td class="table-cell table-body-cell col-email">{{ strtolower( $row['user_email'] ) }}</td>
+      <td class="table-cell table-body-cell col-product">{{ $row['description'] }}</td>
+      <td class="table-cell table-body-cell col-campaign">{{ strtolower( $row['campaign'] ) }}</td>
       <td class="table-cell table-body-cell col-payment">
-        {{ $row['payout_method']; }}
+        {{ $row['payout_method'] }}
         <span class="tooltip" style="display: none;" >
           <p>Date: {{ $row['payout_date'] }}</p>
           <p>Method: {{ $row['payout_method'] }}</p>
