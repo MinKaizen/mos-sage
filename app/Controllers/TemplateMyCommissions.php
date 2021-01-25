5ee73bdeead55e78aa1082cc70ae85aaedb2ee73
Martin Cao <martin.g.cao@gmail.com>
2021-01-25 17:28:26 +1100

    Remove dummy return value in controller TemplateMyCommissions:

--- app/Controllers/TemplateMyCommissions.php
+++ app/Controllers/TemplateMyCommissions.php
@@ -19,20 +19,6 @@ class TemplateMyCommissions extends Controller
     }
 
     public function commissions() {
-        return [
-            [
-                'amount' => '$99',
-                'campaign' => 'undefined',
-                'date' => 'undefined',
-                'description' => 'undefined',
-                'display_name' => 'undefined',
-                'payout_address' => 'undefined',
-                'payout_date' => 'undefined',
-                'payout_method' => 'undefined',
-                'payout_transaction_id' => 'undefined',
-                'user_email' => 'undefined',
-            ],
-        ];
         return apply_filters( 'mos_commissions', [] );
     }
 }
