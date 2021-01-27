ea2c05827ffe13236f04a1ac0d3c8debd2695a18
Martin Cao <martin.g.cao@gmail.com>
2021-01-28 10:07:12 +1100

    Add markup for ai-MIS

---
+++ resources/views/partials/ai-MIS.blade.php
@@ -0,0 +1,9 @@
+<div class="ai-MIS">
+  <div class="ai-MIS_Main">
+    <div class="ai-MIS_Name">{{ $name }} ID:</div>
+    <div class="ai-MIS_Value">{{ $value }}</div>
+  </div>
+  <a class="ai-MIS_Main_Link" href="{{ $course_link }}" @new_tab>
+    <img src="@asset('images/icon-redirect-solid.svg')">
+  </a>
+</div>
