5f82d04f9a260b8d9a6f701185944444cfe43cec
Martin Cao <martin.g.cao@gmail.com>
2021-01-28 10:30:22 +1100

    FIX: classname for ai-MIS_Link

--- resources/views/partials/ai-MIS.blade.php
+++ resources/views/partials/ai-MIS.blade.php
@@ -3,7 +3,7 @@
     <div class="ai-MIS_Name">{{ $name }} ID:</div>
     <div class="ai-MIS_Value">{{ $value }}</div>
   </div>
-  <a class="ai-MIS_Main_Link" href="{{ $course_link }}" @new_tab>
+  <a class="ai-MIS_Link" href="{{ $course_link }}" @new_tab>
     <img src="@asset('images/icon-redirect-solid.svg')">
   </a>
 </div>
