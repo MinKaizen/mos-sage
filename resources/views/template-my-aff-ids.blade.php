59ac36c060744598724605cce6b4b0525a0f4052
Martin Cao <martin.g.cao@gmail.com>
2021-01-28 10:07:25 +1100

    Add markup for template-my-aff-ids

--- resources/views/template-my-aff-ids.blade.php
+++ resources/views/template-my-aff-ids.blade.php
@@ -5,5 +5,11 @@
 @extends('layouts.main')
 
 @section('content')
-  <h1>This is the My Affiliate IDs Page</h1>
+  <div class="aim-AffIdsMain">
+    <div class="aim-AffIdsMain_Container">
+      <h2 class="aim-AffIdsMain_Title">My Affiliate IDs</h2>
+      <p class="aim-AffIdsMain_Paragraph"><strong>IMPORTANT:</strong> Please double check your IDs below because if a field is blank or incorrect, you will not earn for that affiliate program and we cannot help you. If you need to edit any of the Ds, click on the blue arrow icon which will take you to the original page with instructions. </p>
+      @include('partials.ai-AffIds', ['user_mis' => $user_mis])
+    </div>
+  </div>
 @endsection
