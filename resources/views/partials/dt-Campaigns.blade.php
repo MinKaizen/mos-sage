2f3d9e8ee17f2aa6a8e6122929686cfe8e8f33d4
Martin Cao <martin.g.cao@gmail.com>
2021-01-27 17:46:53 +1100

    Add classnames to dt-Campaigns markup

--- resources/views/partials/dt-Campaigns.blade.php
+++ resources/views/partials/dt-Campaigns.blade.php
@@ -1,21 +1,21 @@
-<table class='js-MosTable'>
-  <thead>
-    <tr>
-      <th>Name</th>
-      <th>Clicks</th>
-      <th>Partners</th>
-      <th>Commissions</th>
-      <th>EPC</th>
+<table class='dt-Table dt-Table-campaigns js-MosTable'>
+  <thead class="dt-Head dt-Head-campaigns">
+    <tr class="dt-Row dt-HeaderRow dt-Row-campaigns dt-HeaderRow-campaigns">
+      <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-name">Name</th>
+      <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-clicks">Clicks</th>
+      <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-partners">Partners</th>
+      <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-commissions">Commissions</th>
+      <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-epc">EPC</th>
     </tr>
   </thead>
-  <tbody>
+  <tbody class="dt-Body dt-Body-campaigns">
     @foreach($campaigns as $campaign)
-      <tr>
-        <td>{{ $campaign['name'] }}</td>
-        <td>{{ $campaign['clicks'] }}</td>
-        <td>{{ $campaign['partners'] }}</td>
-        <td>{{ $campaign['commissions_formatted'] }}</td>
-        <td>{{ $campaign['epc_formatted'] }}</td>
+      <tr class="dt-Row dt-Row-campaigns">
+        <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-name">{{ $campaign['name'] }}</td>
+        <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-clicks">{{ $campaign['clicks'] }}</td>
+        <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-partners">{{ $campaign['partners'] }}</td>
+        <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-commissions">{{ $campaign['commissions_formatted'] }}</td>
+        <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-epc">{{ $campaign['epc_formatted'] }}</td>
       </tr>
     @endforeach
   </tbody>
