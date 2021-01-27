7bd72fa9c992785b01111dc2404585cc7cefb295
Martin Cao <martin.g.cao@gmail.com>
2021-01-27 16:37:28 +1100

    Wrap total elements in Totals wrapper in te-TotalEarnings

--- resources/views/partials/te-TotalEarnings.blade.php
+++ resources/views/partials/te-TotalEarnings.blade.php
@@ -1,19 +1,21 @@
 <div class="te-TotalEarnings">
   <h2 class="te-TotalEarnings_Title">@shortcode('[mos_name]')'s Verified Earnings</h2>
-  <div class="te-TotalEarnings_Total">
-    <span class="te-TotalEarnings_Total_Currency">$</span>
-    <span class="te-TotalEarnings_Total_Number">{{ $total_commissions }}</span>
-    <span class="te-TotalEarnings_Total_Label">Commissions</span>
-  </div>
-  <div class="te-TotalEarnings_Total">
-    <span class="te-TotalEarnings_Total_Currency">$</span>
-    <span class="te-TotalEarnings_Total_Number">{{ $total_referrals }}</span>
-    <span class="te-TotalEarnings_Total_Label">Referrals</span>
-  </div>
-  <div class="te-TotalEarnings_Total">
-    <span class="te-TotalEarnings_Total_Currency">$</span>
-    <span class="te-TotalEarnings_Total_Number">{{ $total_epr }}</span>
-    <span class="te-TotalEarnings_Total_Label">EPR</span>
+  <div class="te-TotalEarnings_Totals">
+    <div class="te-TotalEarnings_Total">
+      <span class="te-TotalEarnings_Total_Currency">$</span>
+      <span class="te-TotalEarnings_Total_Number">{{ $total_commissions }}</span>
+      <span class="te-TotalEarnings_Total_Label">Commissions</span>
+    </div>
+    <div class="te-TotalEarnings_Total">
+      <span class="te-TotalEarnings_Total_Currency">$</span>
+      <span class="te-TotalEarnings_Total_Number">{{ $total_referrals }}</span>
+      <span class="te-TotalEarnings_Total_Label">Referrals</span>
+    </div>
+    <div class="te-TotalEarnings_Total">
+      <span class="te-TotalEarnings_Total_Currency">$</span>
+      <span class="te-TotalEarnings_Total_Number">{{ $total_epr }}</span>
+      <span class="te-TotalEarnings_Total_Label">EPR</span>
+    </div>
   </div>
   <img src="@asset('images/logo-type-light.svg')" alt="My Online Startup" class="te-TotalEarnings_Logo" width="208" height="37">
 </div>
