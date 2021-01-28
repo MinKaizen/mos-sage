23191c938106c6737397cd7b292f085f5f528f26
Martin Cao <martin.g.cao@gmail.com>
2021-01-28 11:44:13 +1100

    Enqueue login.css on login page

--- app/admin.php
+++ app/admin.php
@@ -22,3 +22,13 @@ add_action('customize_register', function (\WP_Customize_Manager $wp_customize) 
 add_action('customize_preview_init', function () {
     wp_enqueue_script('sage/customizer.js', asset_path('scripts/customizer.js'), ['customize-preview'], null, true);
 });
+
+
+add_action('login_enqueue_scripts', function () {
+    wp_enqueue_style(
+        'mos-sage/login.css',
+        asset_path('styles/login.css'),
+        false,
+        null
+    );
+}, 100);
