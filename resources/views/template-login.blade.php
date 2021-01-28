ab2b1a15b36accf749748bb34414f50a2d1294a2
Martin Cao <martin.g.cao@gmail.com>
2021-01-28 18:33:15 +1100

    Wrap login form in lg-Main

--- resources/views/template-login.blade.php
+++ resources/views/template-login.blade.php
@@ -5,13 +5,7 @@
 @extends('layouts.login')
 
 @section('content')
-  @shortcode('[gravityform action="login"
-                           title="false"
-                           description="false"
-                           logged_in_message="You are already logged in"
-                           registration_link_text="Sign Up Instead"
-                           logged_in_avatar="false"
-                           logout_redirect="/login"
-                           login_redirect="/courses/free-course"
-                           forgot_password_text="Reset Password" /]')
+  <main class="lg-Main">
+    @shortcode('[gravityform action="login" title="false" description="false" logged_in_message="You are already logged in" registration_link_text="Sign Up Instead" logged_in_avatar="false" logout_redirect="/login" login_redirect="/courses/free-course" forgot_password_text="Reset Password" /]')
+  </main>
 @endsection
