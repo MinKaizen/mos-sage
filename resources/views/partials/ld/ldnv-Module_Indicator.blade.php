ecf51207a0cd55f5d170ca7d5206547b50a1d334
Martin Cao <martin.g.cao@gmail.com>
2021-01-29 17:07:39 +1100

    Refactor module indicators to use sass instead of inline

--- resources/views/partials/ld/ldnv-Module_Indicator.blade.php
+++ resources/views/partials/ld/ldnv-Module_Indicator.blade.php
@@ -1,9 +1,9 @@
 @if($module_indicator == 'partner')
-  <img class="ldnv-Module_Indicator ldnv-Module_Indicator-partner" src="@asset('images/icon-partner-module-hollow.png')">
+  <span class="ldnv-Module_Indicator ldnv-Module_Indicator-partner"></span>
 @else
   @if($complete)
-    <img class="ldnv-Module_Indicator ldnv-Module_Indicator-complete" src="@asset('images/icon-lesson-complete-hollow.png')" alt="(Complete)">
+    <span class="ldnv-Module_Indicator ldnv-Module_Indicator-complete"></span>
   @else
-    <img class="ldnv-Module_Indicator ldnv-Module_Indicator-incomplete" src="@asset('images/icon-lesson-incomplete-hollow.png')" alt="(Incomplete)">
+    <span class="ldnv-Module_Indicator ldnv-Module_Indicator-incomplete"></span>
   @endif
 @endif
