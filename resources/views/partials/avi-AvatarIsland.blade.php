47341966972cc99e2a43f868a5da9b2da5b9104d
Martin Cao <martin.g.cao@gmail.com>
2021-01-29 18:54:10 +1100

    Markup avi-AvatarIsland

---
+++ resources/views/partials/avi-AvatarIsland.blade.php
@@ -0,0 +1,16 @@
+<div class="avi-AvatarIsland">
+  <div class="avi-AvatarWrapper">
+    @shortcode('[avatar]')
+  </div>
+  <div class="avi-AvatarUpload">
+    @shortcode('[avatar_upload]')
+  </div>
+  <div class="avi-Details">
+    <div class="avi-Details_Label avi-Details_Label-username">Username:</div>
+    <div class="avi-Details_Value avi-Details_Value-username">@shortcode('[mos_username]')</div>
+    <div class="avi-Details_Label avi-Details_Label-Level">Level:</div>
+    <div class="avi-Details_Value avi-Details_Value-Level">@shortcode('[mos_level]')</div>
+    <div class="avi-Details_Label avi-Details_Label-Progress">Course Status:</div>
+    <div class="avi-Details_Value avi-Details_Value-Progress">@shortcode('[mos_progress]')</div>
+  </div>
+</div>
