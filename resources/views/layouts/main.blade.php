59b8be091e3c4282c8292b44c243df4f2be5c916
Martin Cao <martin.g.cao@gmail.com>
2021-01-29 15:53:36 +1100

    Set bd-Body background via sass instead of html

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,6 @@
   <body @php body_class('bd-Body') @endphp
     data-level="{{ apply_filters('mos_user_level_slug', '') }}"
     data-next-level="{{ apply_filters('mos_user_next_level_slug', '') }}"
-    style="background: url(@asset('images/background-geometric.svg')) #202942; background-size: contain"
   >
     @include('partials.mn-MobileNav', ['menu_slug' => $top_menu_slug])
     @include('partials.hd-Header')
