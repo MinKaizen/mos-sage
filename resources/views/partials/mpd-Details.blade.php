68e62eebbc6fea9d80af2573086fad7f6f81d592
Martin Cao <martin.g.cao@gmail.com>
2021-02-01 06:42:14 +1100

    Remove ajax=true from mpd-Details shortcode

--- resources/views/partials/mpd-Details.blade.php
+++ resources/views/partials/mpd-Details.blade.php
@@ -1,5 +1,5 @@
 @if(isset($details_gform_id))
   <div class="mpd-Details js-ProfileDetails">
-    @shortcode("[gravityform id=$details_gform_id title=false description=false ajax=true]")
+    @shortcode("[gravityform id=$details_gform_id title=false description=false]")
   </div>
 @endif
