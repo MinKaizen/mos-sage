9d5e6d817f9a22cf540907e1ac274ee80478feb1
Martin Cao <martin.g.cao@gmail.com>
2021-02-01 10:05:38 +1100

    Refactor ActionPlan: move title into top bar

--- resources/views/partials/ld/ldap-ActionPlan.blade.php
+++ resources/views/partials/ld/ldap-ActionPlan.blade.php
@@ -1,20 +1,21 @@
 <div class="ldap-ActionPlan ldap-ActionPlan-{{ $island_color }}">
 
-  <h2 class="ldap-ActionPlan_Title">Action Plan</h2>
+  <h2 class="ldap-ActionPlan_Title ldap-ActionPlan_Title-{{ $island_color }}">Your Action Plan:</h2>
 
-  @if(isset($action_plan) && $action_plan)
-    @foreach($action_plan as $action_item)
-      @include('partials.ld.ldap-ActionItem', [
-        'title' => isset($action_item->title) ? $action_item->title : '',
-        'description' => isset($action_item->description) ? $action_item->description : '',
-        'cta' => isset($action_item->cta) ? $action_item->cta : (object) [
-          'color' => 'red',
-          'text' => 'Button Text',
-          'url' => '/',
-        ],
-      ])
-    @endforeach
-  @endif
+  <div class="ldap-ActionPlan_Inner">
+    @if(isset($action_plan) && $action_plan)
+      @foreach($action_plan as $action_item)
+        @include('partials.ld.ldap-ActionItem', [
+          'title' => isset($action_item->title) ? $action_item->title : '',
+          'description' => isset($action_item->description) ? $action_item->description : '',
+          'cta' => isset($action_item->cta) ? $action_item->cta : (object) [
+            'color' => 'red',
+            'text' => 'Button Text',
+            'url' => '/',
+          ],
+        ])
+      @endforeach
+    @endif
 
     @if($append_complete)
       <section class="ldap-ActionItem">
