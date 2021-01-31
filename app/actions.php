0e3936f188a3560af084e8befe0609df07560800
Martin Cao <martin.g.cao@gmail.com>
2021-02-01 08:28:31 +1100

    Create ACF options page

--- app/actions.php
+++ app/actions.php
@@ -16,3 +16,16 @@ add_action( 'template_redirect', function() {
 // Remove wrappers around Wordpress User Avatar
 remove_action('wpua_before_avatar', 'wpua_do_before_avatar');
 remove_action('wpua_after_avatar', 'wpua_do_after_avatar');
+
+// Create ACF Options page
+if( function_exists('acf_add_options_page') ) {
+
+	acf_add_options_page(array(
+		'page_title' 	=> 'MOS Settings',
+		'menu_title'	=> 'MOS Settings',
+		'menu_slug' 	=> 'mos-settings',
+		'capability'	=> 'edit_posts',
+		'redirect'		=> true
+	));
+
+}
