c2d01198bca31a1db5bc6a5e03f443b5fa1180c9
Martin Cao <martin.g.cao@gmail.com>
2021-02-01 19:04:06 +1100

    Refactor ms-Info into ms-MySponsor_DetailsIsland

--- resources/views/template-my-sponsor.blade.php
+++ resources/views/template-my-sponsor.blade.php
@@ -13,17 +13,17 @@
       </div>
     </div>
 
-    <div class="msi-Info">
-      <h3 class="msi-Info_Title">Contact Your Sponsor</h3>
-      <div class="msi-Info_Details">
-        <span class="msi-Info_Details_Label">First</span>
-        <span class="msi-Info_Details_Value">{{ $sponsor->first_name }}</span>
-        <span class="msi-Info_Details_Label">Last</span>
-        <span class="msi-Info_Details_Value">{{ $sponsor->last_name }}</span>
-        <span class="msi-Info_Details_Label">Email</span>
-        <span class="msi-Info_Details_Value">{{ $sponsor->user_email }}</span>
-        <span class="msi-Info_Details_Label">Facebook</span>
-        <span class="msi-Info_Details_Value">{{ $sponsor->user_url}}</span>
+    <div class="ms-MySponsor_DetailsIsland">
+      <h3 class="ms-MySponsor_Title">Contact Your Sponsor</h3>
+      <div class="ms-MySponsor_Details">
+        <span class="ms-MySponsor_Details_Label">First</span>
+        <span class="ms-MySponsor_Details_Value">{{ $sponsor->first_name }}</span>
+        <span class="ms-MySponsor_Details_Label">Last</span>
+        <span class="ms-MySponsor_Details_Value">{{ $sponsor->last_name }}</span>
+        <span class="ms-MySponsor_Details_Label">Email</span>
+        <span class="ms-MySponsor_Details_Value">{{ $sponsor->user_email }}</span>
+        <span class="ms-MySponsor_Details_Label">Facebook</span>
+        <span class="ms-MySponsor_Details_Value">{{ $sponsor->user_url}}</span>
       </div>
     </div>
 
