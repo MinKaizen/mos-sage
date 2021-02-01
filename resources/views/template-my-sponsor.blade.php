54cbdae4ab7f424d80617304044a1bc240ba88a2
Martin Cao <martin.g.cao@gmail.com>
2021-02-01 13:23:41 +1100

    Rename msd-Details to msi-Info

--- resources/views/template-my-sponsor.blade.php
+++ resources/views/template-my-sponsor.blade.php
@@ -13,17 +13,17 @@
       </div>
     </div>
 
-    <div class="msd-Info">
-      <h3 class="msd-Info_Title">Contact Your Sponsor</h3>
-      <div class="msd-Info_Details">
-        <span class="msd-Info_Details_Label">First</span>
-        <span class="msd-Info_Details_Value">{{ $sponsor->first_name }}</span>
-        <span class="msd-Info_Details_Label">Last</span>
-        <span class="msd-Info_Details_Value">{{ $sponsor->last_name }}</span>
-        <span class="msd-Info_Details_Label">Email</span>
-        <span class="msd-Info_Details_Value">{{ $sponsor->user_email }}</span>
-        <span class="msd-Info_Details_Label">Facebook</span>
-        <span class="msd-Info_Details_Value">{{ $sponsor->user_url}}</span>
+    <div class="msi-Info">
+      <h3 class="msi-Info_Title">Contact Your Sponsor</h3>
+      <div class="msi-Info_Details">
+        <span class="msi-Info_Details_Label">First</span>
+        <span class="msi-Info_Details_Value">{{ $sponsor->first_name }}</span>
+        <span class="msi-Info_Details_Label">Last</span>
+        <span class="msi-Info_Details_Value">{{ $sponsor->last_name }}</span>
+        <span class="msi-Info_Details_Label">Email</span>
+        <span class="msi-Info_Details_Value">{{ $sponsor->user_email }}</span>
+        <span class="msi-Info_Details_Label">Facebook</span>
+        <span class="msi-Info_Details_Value">{{ $sponsor->user_url}}</span>
       </div>
     </div>
 
