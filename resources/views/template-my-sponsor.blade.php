23ed3228d97e5a3df106e68cec63fe7f36c4b604
Martin Cao <martin.g.cao@gmail.com>
2021-02-01 13:04:52 +1100

    Markup template-my-sponsor

--- resources/views/template-my-sponsor.blade.php
+++ resources/views/template-my-sponsor.blade.php
@@ -5,5 +5,27 @@
 @extends('layouts.main')
 
 @section('content')
-  <h1>This is the My Sponsor Page</h1>
+  <div class="ms-MySponsor">
+
+    <div class="msai-AvatarIsland">
+      <div class="msai-AvatarWrapper">
+        {!! get_avatar( $sponsor->ID, 200 ) !!}
+      </div>
+    </div>
+
+    <div class="msd-Info">
+      <h3 class="msd-Info_Title">Contact Your Sponsor</h3>
+      <div class="msd-Info_Details">
+        <span class="msd-Info_Details_Label">First</span>
+        <span class="msd-Info_Details_Value">{{ $sponsor->first_name }}</span>
+        <span class="msd-Info_Details_Label">Last</span>
+        <span class="msd-Info_Details_Value">{{ $sponsor->last_name }}</span>
+        <span class="msd-Info_Details_Label">Email</span>
+        <span class="msd-Info_Details_Value">{{ $sponsor->user_email }}</span>
+        <span class="msd-Info_Details_Label">Facebook</span>
+        <span class="msd-Info_Details_Value">{{ $sponsor->user_url}}</span>
+      </div>
+    </div>
+
+  </div>
 @endsection
