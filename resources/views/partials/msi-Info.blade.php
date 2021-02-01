423a7c68b89da314de2af1f76598486214d45c50
Martin Cao <martin.g.cao@gmail.com>
2021-02-01 18:19:26 +1100

    Move msi-Info into its own partial

---
+++ resources/views/partials/msi-Info.blade.php
@@ -0,0 +1,13 @@
+<div class="msi-Info">
+  <h3 class="msi-Info_Title">Contact Your Sponsor</h3>
+  <div class="msi-Info_Details">
+    <span class="msi-Info_Details_Label">First</span>
+    <span class="msi-Info_Details_Value">{{ $sponsor->first_name }}</span>
+    <span class="msi-Info_Details_Label">Last</span>
+    <span class="msi-Info_Details_Value">{{ $sponsor->last_name }}</span>
+    <span class="msi-Info_Details_Label">Email</span>
+    <span class="msi-Info_Details_Value">{{ $sponsor->user_email }}</span>
+    <span class="msi-Info_Details_Label">Facebook</span>
+    <span class="msi-Info_Details_Value">{{ $sponsor->user_url}}</span>
+  </div>
+</div>
