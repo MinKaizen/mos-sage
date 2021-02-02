21b9103dd49e848681b3d8e9e9e8c3b04276c202
Martin Cao <martin.g.cao@gmail.com>
2021-02-02 15:41:50 +1100

    Refactor page.blade markup

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
   @while(have_posts()) @php the_post() @endphp
-    @include('partials.pgc-Content')
+    <div class="p-Page">
+      @php the_content() @endphp
+
+      @if(isset($show_sidebar) && $show_sidebar)
+        <aside class="pgsb-Sidebar">
+          @php dynamic_sidebar($mos_sidebar); @endphp
+          @php dynamic_sidebar('affiliate_sidebar'); @endphp
+        </aside>
+      @endif
+    </div>
   @endwhile
 @endsection
