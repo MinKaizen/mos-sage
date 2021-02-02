7ee68f9dc7a410852862cdc4499d4a5282b0e867
Martin Cao <martin.g.cao@gmail.com>
2021-02-03 07:27:00 +1100

    Wrap page template with Text Island

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -3,14 +3,17 @@
 @section('content')
   @while(have_posts()) @php the_post() @endphp
     <div class="p-Page">
-      @php the_content() @endphp
+      <div class="ti-TextIsland c-Content">
+        <h2>{{ the_title() }}</h2>
+        @php the_content() @endphp
+      </div>
 
-      @if(isset($show_sidebar) && $show_sidebar)
+      {{-- @if(isset($show_sidebar) && $show_sidebar)
         <aside class="pgsb-Sidebar">
           @php dynamic_sidebar($mos_sidebar); @endphp
           @php dynamic_sidebar('affiliate_sidebar'); @endphp
         </aside>
-      @endif
+      @endif --}}
     </div>
   @endwhile
 @endsection
