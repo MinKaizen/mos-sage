8be59365a7ea86ef7abb4c5473872ee3b1c6d6fc
Martin Cao <martin.g.cao@gmail.com>
2021-02-04 11:15:37 +1100

    Style mf-MisForm

--- resources/views/partials/mf-MisForm.blade.php
+++ resources/views/partials/mf-MisForm.blade.php
@@ -1,9 +1,14 @@
-<form action="{{ esc_url( admin_url('admin-post.php') ) }}" method="POST">
-  <div class="mf-Field">
-    <label for="" class="mf-Label">{{ $mis->name }}</label>
-    <input type="text" name="value" value="{{ $current_value }}">
+<form class="mf-MisForm"
+      action="{{ esc_url( admin_url('admin-post.php') ) }}"
+      method="POST">
+  <div class="mf-Body @if(isset($_GET['mis_saved_' . $mis->slug]) && $_GET['mis_saved_' . $mis->slug] )mf-Body-saved @endif">
+    <div class="mf-Fields">
+      <label class="mf-Label" for="" class="mf-Label">{{ $mis->name }} ID:</label>
+      <input class="mf-Input"type="text" name="value" value="{{ $current_value }}">
+      <input class="mf-Submit" type="submit" value="Save">
+    </div>
     @if(isset($_GET['mis_saved_' . $mis->slug]) && $_GET['mis_saved_' . $mis->slug] )
-      <div class="mf-Message">Saved!</div>
+      <span class="mf-Saved">✓Saved</span>
     @endif
   </div>
   {!! wp_nonce_field("save_mis_$mis->slug") !!}
@@ -12,5 +17,4 @@
   <input type="hidden" name="meta_key" value="{{ $mis->meta_key }}">
   <input type="hidden" name="redirect" value="{{ add_query_arg( 'mis_saved_' . $mis->slug, 1, get_permalink() ) }}">
   <input type="hidden" name="action" value="update_user_mis">
-  <input class="mf-Submit" type="submit" value="submit">
 </form>
