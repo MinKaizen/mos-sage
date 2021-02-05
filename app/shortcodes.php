3243c10c75911cc0a80b4d0cb61e243b5f780a02
Martin Cao <martin.g.cao@gmail.com>
2021-02-05 14:38:58 +1100

    Use filters and shortcodes in shortcode mos_mis_form instead of MOS\Affiliate\MIS class

--- app/shortcodes.php
+++ app/shortcodes.php
@@ -2,23 +2,19 @@
 
 namespace App;
 
-use \MOS\Affiliate\MIS;
-
-use function \App\get_mis;
-
 // Shortcode for mis form
 add_shortcode( 'mos_mis_form', function( $atts ) {
     if ( !isset( $atts['network'] ) ) {
         return '[no network specified]';
     }
 
-    $mis = new MIS( $atts['network'] );
+    $mis = apply_filters( 'mos_mis_object', null, $atts['network'] );
 
-    if ( !$mis->exists ) {
+    if ( $mis==null || !$mis->exists ) {
         return "[no MIS with slug $atts[network]]";
     }
 
-    $current_value = get_mis( $atts['network'] );
+    $current_value = do_shortcode( "[mos_mis network='$atts[network]']" );
 
     $vars = [
         'mis' => $mis,
