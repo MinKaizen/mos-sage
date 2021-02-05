75036dc2fb40fb1cebfabc398cecee8548491fbe
Martin Cao <martin.g.cao@gmail.com>
2021-02-05 14:54:08 +1100

    Use filters instead of MOS\Affiliate\Users class in TemplateMyAffids

--- app/Controllers/TemplateMyAffIds.php
+++ app/Controllers/TemplateMyAffIds.php
@@ -3,25 +3,24 @@
 namespace App\Controllers;
 
 use Sober\Controller\Controller;
-use MOS\Affiliate\User;
 
 class TemplateMyAffIds extends Controller {
 
     public function userMis() {
         $all_mis = apply_filters( 'mos_all_mis', ['Nothing yet'] );
         $user_mis = [];
-        $user = User::current();
 
         if ( empty( $all_mis ) ) {
             return [];
         }
 
         foreach ( $all_mis as $mis ) {
-            if ( $user->qualifies_for_mis( $mis->slug ) ) {
+            $qualifies_for_mis = apply_filters( 'mos_qualifies_for_mis', false, $mis->slug );
+            if ( $qualifies_for_mis ) {
                 $user_mis[$mis->slug] = [
                     'name' => $mis->name,
                     'course_link' => $mis->course_link,
-                    'value' => $user->get_mis( $mis->slug ),
+                    'value' => do_shortcode("[mos_mis network=$mis->slug]"),
                 ];
             }
         }
