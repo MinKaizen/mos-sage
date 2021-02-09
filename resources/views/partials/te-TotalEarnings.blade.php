8bdcd1b81ab9695651d9cd37a8107353969f3dd3
Martin Cao <martin.g.cao@gmail.com>
2021-02-10 06:54:18 +1100

    Remove Referrals and EPR from te-TotalEarnings

--- resources/views/partials/te-TotalEarnings.blade.php
+++ resources/views/partials/te-TotalEarnings.blade.php
@@ -4,9 +4,9 @@
     <div class="te-TotalEarnings_Total">
       <span class="te-TotalEarnings_Total_Currency">$</span>
       <span class="te-TotalEarnings_Total_Number">{{ $total_commissions }}</span>
-      <span class="te-TotalEarnings_Total_Label">Commissions</span>
+      {{-- <span class="te-TotalEarnings_Total_Label">Commissions</span> --}}
     </div>
-    <div class="te-TotalEarnings_Total">
+    {{-- <div class="te-TotalEarnings_Total">
       <span class="te-TotalEarnings_Total_Currency">#</span>
       <span class="te-TotalEarnings_Total_Number">{{ $total_referrals }}</span>
       <span class="te-TotalEarnings_Total_Label">Referrals</span>
@@ -15,7 +15,7 @@
       <span class="te-TotalEarnings_Total_Currency">$</span>
       <span class="te-TotalEarnings_Total_Number">{{ $total_epr }}</span>
       <span class="te-TotalEarnings_Total_Label">EPR</span>
-    </div>
+    </div> --}}
   </div>
   <img src="@asset('images/logo-type-light.svg')" alt="My Online Startup" class="te-TotalEarnings_Logo" width="208" height="37">
 </div>
