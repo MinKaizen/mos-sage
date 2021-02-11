8ce4530a412f5b172f425db9592a979ff65cb243
Martin Cao <martin.g.cao@gmail.com>
2021-02-12 09:48:12 +1100

    Add -error class on mf-MisForm body if an error occurred

--- resources/views/partials/mf-MisForm.blade.php
+++ resources/views/partials/mf-MisForm.blade.php
@@ -1,7 +1,7 @@
 <form class="mf-MisForm"
       action="{{ esc_url( admin_url('admin-post.php') ) }}"
       method="POST">
-  <div class="mf-Body @if($is_saved)mf-Body-saved @endif">
+  <div class="mf-Body @if($is_saved)mf-Body-saved @endif @if($is_error)mf-Body-error @endif">
     <div class="mf-Fields">
       <label class="mf-Label" for="" class="mf-Label">{{ $mis->name }} ID:</label>
       <input class="mf-Input"type="text" name="value" value="{{ $current_value }}">
