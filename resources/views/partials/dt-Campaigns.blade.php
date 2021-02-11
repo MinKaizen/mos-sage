781dea9bebb01bfbb01f1c631136f90c8795268e
Martin Cao <martin.g.cao@gmail.com>
2021-02-11 11:30:48 +1100

    Set column class names in dt-Campaigns

--- resources/views/partials/dt-Campaigns.blade.php
+++ resources/views/partials/dt-Campaigns.blade.php
@@ -3,13 +3,13 @@
     <tr class="dt-Row dt-HeaderRow dt-Row-campaigns dt-HeaderRow-campaigns">
       <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-name">Name</th>
       <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-clicks">Clicks</th>
-      <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-partners">Monthly P.</th>
-      <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-partners">Yearly P.</th>
-      <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-partners">Lifetime P.</th>
-      <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-partners">Coaching</th>
-      <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-partners">FBT</th>
-      <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-partners">LGS</th>
-      <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-partners">ABB</th>
+      <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-monthly">Monthly P.</th>
+      <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-yearly">Yearly P.</th>
+      <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-lifetime">Lifetime P.</th>
+      <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-coaching">Coaching</th>
+      <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-fbt">FBT</th>
+      <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-lgs">LGS</th>
+      <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-abb">ABB</th>
       <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-commissions">Commissions</th>
       {{-- <th class="dt-Cell dt-HeaderCell dt-Cell-campaigns dt-HeaderCell-campaigns dt-Col-campaigns-epc">EPC</th> --}}
     </tr>
@@ -19,13 +19,13 @@
       <tr class="dt-Row dt-Row-campaigns">
         <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-name">{{ $campaign['name'] }}</td>
         <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-clicks">{{ $campaign['clicks'] }}</td>
-        <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-partners">{{ $campaign['monthly_partner'] }}</td>
-        <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-partners">{{ $campaign['yearly_partner'] }}</td>
-        <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-partners">{{ $campaign['lifetime_partner'] }}</td>
-        <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-partners">{{ $campaign['coaching'] }}</td>
-        <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-partners">{{ $campaign['fb_toolkit'] }}</td>
-        <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-partners">{{ $campaign['lead_system'] }}</td>
-        <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-partners">{{ $campaign['authority_bonuses'] }}</td>
+        <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-monthly">{{ $campaign['monthly_partner'] }}</td>
+        <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-yearly">{{ $campaign['yearly_partner'] }}</td>
+        <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-lifetime">{{ $campaign['lifetime_partner'] }}</td>
+        <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-coaching">{{ $campaign['coaching'] }}</td>
+        <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-fbt">{{ $campaign['fb_toolkit'] }}</td>
+        <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-lgs">{{ $campaign['lead_system'] }}</td>
+        <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-abb">{{ $campaign['authority_bonuses'] }}</td>
         <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-commissions">{{ $campaign['commissions_formatted'] }}</td>
         {{-- <td class="dt-Cell dt-Cell-campaigns dt-Col-campaigns-epc">{{ $campaign['epc_formatted'] }}</td> --}}
       </tr>
