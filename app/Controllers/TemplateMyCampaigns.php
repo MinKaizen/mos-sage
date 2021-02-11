be2e7ef7e0e624359444f710fee98f96a81ed4d1
Martin Cao <martin.g.cao@gmail.com>
2021-02-11 11:33:52 +1100

    Expose vars showLifetime and showCoaching in controller TemplateMyCampaigns

--- app/Controllers/TemplateMyCampaigns.php
+++ app/Controllers/TemplateMyCampaigns.php
@@ -9,4 +9,15 @@ class TemplateMyCampaigns extends Controller
     public function campaigns() {
         return apply_filters( 'mos_campaigns', [] );
     }
+
+    public function showLifetime(): bool {
+        $has_access_yearly = apply_filters( 'mos_has_access', false, 'yearly_partner', get_current_user_id() );
+        return $has_access_yearly;
+    }
+
+    public function showCoaching(): bool {
+        $has_access_lifetime = apply_filters( 'mos_has_access', false, 'lifetime_partner', get_current_user_id() );
+        return $has_access_lifetime;
+    }
+
 }
