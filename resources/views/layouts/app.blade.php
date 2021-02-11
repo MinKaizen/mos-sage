d337628c9231c2993bbe8bad8b59f9b620b3eb2a
Martin Cao <martin.g.cao@gmail.com>
2021-02-11 11:16:06 +1100

    Use controller var level_slug in app.blade instead of apply_filters()

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html {!! get_language_attributes() !!}>
   @include('partials.head')
   <body @php body_class('bd-Body') @endphp
-    data-level="{{ apply_filters('mos_user_level_slug', '') }}"
+    data-level="{{ $level_slug }}"
     data-next-level="{{ apply_filters('mos_user_next_level_slug', '') }}"
   >
     @include('partials.mn-MobileNav', ['menu_slug' => $top_menu_slug])
