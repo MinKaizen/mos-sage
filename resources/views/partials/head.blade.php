304658e5cfac638b774a1de0b59be5289c24e86b
Martin Cao <martin.g.cao@gmail.com>
2021-02-15 08:02:50 +1100

    Adjust global font

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -7,7 +7,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;700&family=Ubuntu:wght@400;700&display=swap" rel="stylesheet">
 
   {{-- Local fonts --}}
-  <link rel="preconnect" href="{{ home_url('/') }}">
+  {{-- <link rel="preconnect" href="{{ home_url('/') }}">
   <link rel="font" type="font/ttf" href="@asset('fonts/geomanist-regular-webfont.ttf')">
   <link rel="font" type="font/woff" href="@asset('fonts/geomanist-regular-webfont.woff')">
   <link rel="font" type="font/woff2" href="@asset('fonts/geomanist-regular-webfont.woff2')">
@@ -19,5 +19,5 @@
   <link rel="font" type="font/woff2" href="@asset('fonts/geomanist-italic-webfont.woff2')">
   <link rel="font" type="font/ttf" href="@asset('fonts/geomanist-bold-italic-webfont.ttf')">
   <link rel="font" type="font/woff" href="@asset('fonts/geomanist-bold-italic-webfont.woff')">
-  <link rel="font" type="font/woff2" href="@asset('fonts/geomanist-bold-italic-webfont.woff2')">
+  <link rel="font" type="font/woff2" href="@asset('fonts/geomanist-bold-italic-webfont.woff2')"> --}}
 </head>
