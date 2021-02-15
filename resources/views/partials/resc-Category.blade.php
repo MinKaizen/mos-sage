8f3eb8bbc537abe5192d0518f025a02a65fc2578
Martin Cao <martin.g.cao@gmail.com>
2021-02-15 16:33:46 +1100

    Open resources links in new tab

--- resources/views/partials/resc-Category.blade.php
+++ resources/views/partials/resc-Category.blade.php
@@ -2,7 +2,7 @@
   <h3 class="resc-Title">{{ $category }}</h3>
   @foreach($resources as $resource)
     <p class="resc-Resource">
-      <a href="{{ \App\resource_generate_link( $resource->ID ) }}">{{ $resource->post_title }}:</a> {!! wp_strip_all_tags($resource->post_content) !!}
+      <a href="{{ \App\resource_generate_link( $resource->ID ) }}" @new_tab>{{ $resource->post_title }}:</a> {!! wp_strip_all_tags($resource->post_content) !!}
     </p>
   @endforeach
 </div>
