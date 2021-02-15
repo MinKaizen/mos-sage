2a49a357eced166dfa93d26cefb224f8c29b7472
Martin Cao <martin.g.cao@gmail.com>
2021-02-15 15:37:25 +1100

    Add json folder to webpack build process (copy only)

--- resources/assets/build/config.js
+++ resources/assets/build/config.js
@@ -13,7 +13,7 @@ const rootPath = (userConfig.paths && userConfig.paths.root)
 
 const config = merge({
   open: true,
-  copy: '+(images|fonts)/**/*',
+  copy: '+(images|fonts|json)/**/*',
   proxyUrl: 'http://localhost:3000',
   cacheBusting: '[name]_[hash]',
   paths: {
