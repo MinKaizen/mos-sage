712e0432c751ffffeb2f211608e297f99acf08c1
Martin Cao <martin.g.cao@gmail.com>
2021-02-24 09:17:47 +1100

    Add partial cosn-SecureNotice

---
+++ resources/views/partials/cosn-SecureNotice.blade.php
@@ -0,0 +1,7 @@
+<div class="cosn-SecureNotice">
+  <svg class="cosn-Icon" height="100" width="100" color="white">
+    <use xlink:href="#icon-lock-solid" />
+  </svg>
+  <h3 class="cosn-Title">This Is A 100% Secure Payment</h3>
+  <p class="cosn-Description">All orders are through a very secure network. Your credit card information is never stored in any way. We respect your privacy.</p>
+</div>
