d40255830412934b58e6a66e5f08779645dc45e8
Martin Cao <martin.g.cao@gmail.com>
2021-02-24 18:12:32 +1100

    Restyle mis form based on LC Design

--- resources/views/partials/mf-MisForm.blade.php
+++ resources/views/partials/mf-MisForm.blade.php
@@ -1,14 +1,14 @@
 <form class="mf-MisForm"
       action="{{ esc_url( admin_url('admin-post.php') ) }}"
       method="POST">
-  <div class="mf-Body @if($is_saved)mf-Body-saved @endif @if($is_error)mf-Body-error @endif">
-    <div class="mf-Fields">
-      <label class="mf-Label" for="" class="mf-Label">{{ $mis->name }} ID:</label>
+  <div class="mf-Body">
+    <label class="mf-Label" for="" class="mf-Label">{{ $mis->name }} ID:</label>
+    <div class="mf-Fields @if($is_saved)mf-Fields-saved @endif @if($is_error)mf-Fields-error @endif">
       <input class="mf-Input"type="text" name="value" value="{{ $current_value }}">
       <input class="mf-Submit" type="submit" value="Save">
+      @if($is_saved)<span class="mf-Saved">✓ Saved: {{ $current_value }}<br>If you've made a a mistake, you can simply overwrite it and hit "Save" again.</span>@endif
+      @if($is_error)<span class="mf-Error">✗ {{ $error_message }}</span>@endif
     </div>
-    @if($is_saved)<span class="mf-Saved">✓Saved</span>@endif
-    @if($is_error)<span class="mf-Error">✗ {{ $error_message }}</span>@endif
   </div>
   {!! wp_nonce_field("save_mis_$mis->slug") !!}
   <input type="hidden" name="user_id" value="{{ get_current_user_id() }}">
