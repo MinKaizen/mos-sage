d244258e71f907ab98b8693217c3b48a912982d6
Martin Cao <martin.g.cao@gmail.com>
2021-02-26 08:25:31 +1100

    Show Challenges instead of Lessons in ldnv-Nav depending on variable side_nav_style

--- resources/views/partials/ld/ldnv-Nav.blade.php
+++ resources/views/partials/ld/ldnv-Nav.blade.php
@@ -1,12 +1,26 @@
 <div class="ldnv-Nav">
 @foreach($course_structure as $module)
-  @include('partials.ld.ldnv-Module', [
-    'id' => $module->ID,
-    'module_num' => $loop->iteration,
-    'title' => $module->post_title,
-    'lessons' => $module->lessons,
-    'is_complete' => $module->is_complete,
-    'is_current' => $module_id == $module->ID,
-  ])
+  @if(!empty($side_nav_style) && $side_nav_style == 'challenge')
+    <ol class="ldnv-Days">
+      @foreach($module->lessons as $lesson)
+        @include('partials.ld.ldnv-Day', [
+          'day_num' => $loop->index,
+          'title' => $lesson->post_title,
+          'is_complete' => $lesson->is_complete,
+          'is_current' => $lesson->ID == $lesson_id,
+          'link' => $lesson->link,
+        ])
+      @endforeach
+    </ol>
+  @else
+    @include('partials.ld.ldnv-Module', [
+      'id' => $module->ID,
+      'module_num' => $loop->iteration,
+      'title' => $module->post_title,
+      'lessons' => $module->lessons,
+      'is_complete' => $module->is_complete,
+      'is_current' => $module_id == $module->ID,
+    ])
+  @endif
 @endforeach
 </div>
