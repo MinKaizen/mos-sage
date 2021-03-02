0eb565e6293b74d4a5106f3f8084f149fa2ed190
Martin Cao <martin.g.cao@gmail.com>
2021-03-02 13:24:27 +1100

    Fix fonts

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -7,17 +7,20 @@
   <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;700&family=Ubuntu:wght@400;700&display=swap" rel="stylesheet">
 
   {{-- Local fonts --}}
-  {{-- <link rel="preconnect" href="{{ home_url('/') }}">
-  <link rel="font" type="font/ttf" href="@asset('fonts/geomanist-regular-webfont.ttf')">
-  <link rel="font" type="font/woff" href="@asset('fonts/geomanist-regular-webfont.woff')">
+  <link rel="preconnect" href="{{ home_url('/') }}">
+  {{-- <link rel="font" type="font/ttf" href="@asset('fonts/geomanist-light-webfont.ttf')">
+  <link rel="font" type="font/ttf" href="@asset('fonts/geomanist-light-webfont.woff')"> --}}
+  <link rel="font" type="font/ttf" href="@asset('fonts/geomanist-light-webfont.woff2')">
+  {{-- <link rel="font" type="font/ttf" href="@asset('fonts/geomanist-regular-webfont.ttf')"> --}}
+  {{-- <link rel="font" type="font/woff" href="@asset('fonts/geomanist-regular-webfont.woff')"> --}}
   <link rel="font" type="font/woff2" href="@asset('fonts/geomanist-regular-webfont.woff2')">
-  <link rel="font" type="font/ttf" href="@asset('fonts/geomanist-bold-webfont.ttf')">
-  <link rel="font" type="font/woff" href="@asset('fonts/geomanist-bold-webfont.woff')">
+  {{-- <link rel="font" type="font/ttf" href="@asset('fonts/geomanist-bold-webfont.ttf')"> --}}
+  {{-- <link rel="font" type="font/woff" href="@asset('fonts/geomanist-bold-webfont.woff')"> --}}
   <link rel="font" type="font/woff2" href="@asset('fonts/geomanist-bold-webfont.woff2')">
-  <link rel="font" type="font/ttf" href="@asset('fonts/geomanist-italic-webfont.ttf')">
-  <link rel="font" type="font/woff" href="@asset('fonts/geomanist-italic-webfont.woff')">
+  {{-- <link rel="font" type="font/ttf" href="@asset('fonts/geomanist-italic-webfont.ttf')"> --}}
+  {{-- <link rel="font" type="font/woff" href="@asset('fonts/geomanist-italic-webfont.woff')"> --}}
   <link rel="font" type="font/woff2" href="@asset('fonts/geomanist-italic-webfont.woff2')">
-  <link rel="font" type="font/ttf" href="@asset('fonts/geomanist-bold-italic-webfont.ttf')">
-  <link rel="font" type="font/woff" href="@asset('fonts/geomanist-bold-italic-webfont.woff')">
-  <link rel="font" type="font/woff2" href="@asset('fonts/geomanist-bold-italic-webfont.woff2')"> --}}
+  {{-- <link rel="font" type="font/ttf" href="@asset('fonts/geomanist-bold-italic-webfont.ttf')"> --}}
+  {{-- <link rel="font" type="font/woff" href="@asset('fonts/geomanist-bold-italic-webfont.woff')"> --}}
+  <link rel="font" type="font/woff2" href="@asset('fonts/geomanist-bold-italic-webfont.woff2')">
 </head>
