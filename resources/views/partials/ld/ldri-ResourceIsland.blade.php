355aecd348afec2ce29f73f54ddd94a007cc512b
Martin Cao <martin.g.cao@gmail.com>
2021-03-16 18:27:27 +1100

    Add partial ldri-ResouceIsland

---
+++ resources/views/partials/ld/ldri-ResourceIsland.blade.php
@@ -0,0 +1,10 @@
+<div class="ldri-ResourceIsland">
+  <h2 class="ldri-Title @if(isset($island_color))ldri-Title-{{ $island_color }}@endif">Tools & Resources:</h2>
+  <div class="ldri-Resources">
+    @foreach($resources as $resource)
+      <a href="{{ do_shortcode( $resource->link ) }}" class="ldri-ResourceName">{{ $resource->name }}</a>
+      <span class="ldri-ResourceDescription">{!! $resource->description !!}</span>
+      @if(!$loop->last)<hr class="ldri-Divider">@endif
+    @endforeach
+  </div>
+</div>
