9841e3cc29c42e25d5283d1e50c3d001286ce194
Martin Cao <martin.g.cao@gmail.com>
2021-03-17 10:52:54 +1100

    Add c-Link class to ldri-ResourceName

--- resources/views/partials/ld/ldri-ResourceIsland.blade.php
+++ resources/views/partials/ld/ldri-ResourceIsland.blade.php
@@ -2,7 +2,7 @@
   <h2 class="ldri-Title @if(isset($island_color))ldri-Title-{{ $island_color }}@endif">Tools & Resources:</h2>
   <div class="ldri-Resources">
     @foreach($resources as $resource)
-      <a href="{{ do_shortcode( $resource->link ) }}" class="ldri-ResourceName">{{ $resource->name }}</a>
+      <a href="{{ do_shortcode( $resource->link ) }}" class="ldri-ResourceName c-Link">{{ $resource->name }}</a>
       <span class="ldri-ResourceDescription">{!! $resource->description !!}</span>
       @if(!$loop->last)<hr class="ldri-Divider">@endif
     @endforeach
